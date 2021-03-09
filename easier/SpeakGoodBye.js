5f56550beb1b3a1c40773a6efeb95d8e7fdd8f6c
Hrithik <hrithik.ghanty363@gmail.com>
2021-03-09 21:28:47 +0530

    First commit

---
+++ easier/SpeakGoodBye.js
@@ -0,0 +1,10 @@
+(function (window) {
+    var byespeaker={};
+    var speakword="Good Bye";
+    byespeaker.speak=function (name) {
+        console.log(speakword + " " + name);
+    }
+
+    window.byespeaker=byespeaker;
+
+})(window);
